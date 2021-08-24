9685641d71e1985d3b6e3cb752d21f7a8fc9d8d8
Aubarino <53990174+Aubarino@users.noreply.github.com>
2021-08-25 04:42:50 +1000

    the main c# file

---
+++ AubsAnimationScript.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+///░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░///
+///░░Aubarino's Animation Script░░///
+///░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░///
+///░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░░///
+///░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░///
+///░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░///
+///░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░///
+///░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░///
+///█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█///
+///█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█///
+///░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░///
+///░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░///
+///░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░///
+///░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░///
+///░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░///
+///░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░///
+///░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░///
+///░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░///
+///░░░░░░░░░░░░░░░░░░░░▀▀▀▀▀▀▀▀░░░///
+///░░Welcome to my dogshit code░░░///
+///░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░///
+
+//read the txt that comes with this.
+
+//the namespace of the mod...
+namespace Mod
+{
+    public class AubsAnimationScript : MonoBehaviour
+    {
+        private int AniMax = 10;
+        private float AniDelay = 5;
+        private float AniClock = 99999;
+        private int AniFrame = 0;
+        private Sprite[] AniFramePort;        
+        private GameObject AniInstance;
+
+        //function to set the animation data
+        //format is The Last Frame, Delay inbetween frames, the Texture Rescale to devide by, The Gameobject carry-over...
+        public void AnimationData(int maxFrame, float Delay, GameObject instanceport)
+        {
+        AniMax = maxFrame;
+        AniDelay = Delay;
+        AniInstance = instanceport;
+        }
+
+        //function to import the frame data
+        public void FrameData(Sprite[] FramePort)
+        {
+        AniFramePort = FramePort;        
+        }        
+
+        private void FixedUpdate()
+        {
+        AniClock += 1;
+        //checks if clock is outside delay range, then resets and checks if the max frame is hit, then it restarts the animation
+        if (AniClock >= AniDelay){
+            AniClock = 0;
+            if (AniFrame >= AniMax){
+                AniFrame = 0;
+                }else{
+                    AniInstance.GetComponent<SpriteRenderer>().sprite = (AniFramePort[AniFrame]);                    
+                    AniFrame += 1;
+                    //sets the frame
+                    }
+            }
+        }
+    }
+}
