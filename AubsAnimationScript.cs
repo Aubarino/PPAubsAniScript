7e4b1e4b04865a6222f527192f1b858e0d1c01f0
Aubarino <53990174+Aubarino@users.noreply.github.com>
2021-08-25 14:29:55 +1000

    updated the script to be more of an API style thing, with multiple Extra Features for scripted event integration and customization

--- AubsAnimationScript.cs
+++ AubsAnimationScript.cs
@@ -34,9 +34,12 @@ namespace Mod
         private int AniMax = 10;
         private float AniDelay = 5;
         private float AniClock = 99999;
-        private int AniFrame = 0;
-        private Sprite[] AniFramePort;        
+        public int AniFrame = 0;
+        private Sprite[] AniFramePort;
         private GameObject AniInstance;
+        public bool ExperimentalColCyc = false;
+        public bool PauseAnimation = false;
+        public bool ResetWhenPause = false;
 
         //function to set the animation data
         //format is The Last Frame, Delay inbetween frames, the Texture Rescale to devide by, The Gameobject carry-over...
@@ -51,24 +54,48 @@ namespace Mod
         public void FrameData(Sprite[] FramePort)
         {
         AniFramePort = FramePort;        
+        }
+
+        //internal function of setting the frame
+        public void InternalSetFrame(Sprite[] InternalAniFramePort, int InternalFrameSet)
+        {
+        AniInstance.GetComponent<SpriteRenderer>().sprite = (InternalAniFramePort[InternalFrameSet]);
+        // ^ ^ ^ sets the frame sprite itself
+        // v v v experimental 'n extra options
+            if (ExperimentalColCyc) {
+            AniInstance.FixColliders(); //resets the colliders per frame, making the collider update with the animation!
+            }
         }        
 
         private void FixedUpdate()
         {
-        AniClock += 1;
-        //checks if clock is outside delay range, then resets and checks if the max frame is hit, then it restarts the animation
-        if (AniClock >= AniDelay){
-            AniClock = 0;
-            if (AniFrame > (AniMax - 1)){
-                AniFrame = 0;
-                AniClock = AniDelay;
-                }else{
-                    AniInstance.GetComponent<SpriteRenderer>().sprite = (AniFramePort[AniFrame]);                    
-                    //debug function (debug only)
-                    //Debug.Log("AniFrame " + (AniFrame + 1));
-                    AniFrame += 1;
-                    // ^ ^ ^ sets the frame
-                    }
+
+        if (PauseAnimation && (AniClock == 0)) {
+        
+        }else{
+            AniClock += 1;
+                //checks if clock is outside delay range, then resets and checks if the max frame is hit, then it restarts the animation
+                if (AniClock >= AniDelay){
+                    AniClock = 0;
+                    if (AniFrame > (AniMax - 1)){
+                        AniFrame = 0;
+                        AniClock = AniDelay;
+                        }else{
+                            if (PauseAnimation) {
+                                    if (ResetWhenPause) {
+                                    AniFrame = 0;
+                                    }                                
+                                InternalSetFrame(AniFramePort, AniFrame);
+                                AniClock = 0;
+                                }else{
+                                    InternalSetFrame(AniFramePort, AniFrame);
+                                    
+                                    //debug function (debug only)
+                                    //Debug.Log("AniFrame " + (AniFrame + 1));
+                                    AniFrame += 1;
+                                }
+                            }
+                }
             }
         }
     }
